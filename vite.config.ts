624492664d97c52f99b9beb21e23bc912ecbb73c
Rafael Freitas <rafaelmf1801@gmail.com>
2025-06-24 17:15:21 -0300

    feat: test new base config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: mode === "production" ? "/grafeno-engenharia-digital/" : "/",
+  base: "/grafeno-engenharia-digital/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
