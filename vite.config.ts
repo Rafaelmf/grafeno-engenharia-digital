58ae28f0712d75f59a4f9a5bbcce45b81e5be7b0
Rafael Freitas <rafaelmf1801@gmail.com>
2025-06-24 17:40:17 -0300

    feat: using base as repo name

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), mode === "development" && componentTagger()].filter(
       Boolean
     ),
-    base: mode === "development" ? "/" : "/grafeno-engenharia-digital/",
+    base: "/grafeno-engenharia-digital/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
